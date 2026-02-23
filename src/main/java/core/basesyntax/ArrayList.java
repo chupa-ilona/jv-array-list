157f92cde5563aad58811c2238cf0d683f820268
IlonaChupa <ilonachupa@gmail.com>
2026-02-23 19:55:52 +0200

    done

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -4,77 +4,94 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
-    //private static final int DEFAULT_LOAD_FACTOR = 75;
+
     private T[] elements;
     private int size;
-    private int capacity;
-    private int loadFactor;
 
     public ArrayList() {
         elements = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
     public ArrayList(int capacity) {
-        elements = (T[]) new Object[capacity];
+        if (capacity <= 0) {
+            elements = (T[]) new Object[DEFAULT_CAPACITY];
+        } else {
+            elements = (T[]) new Object[capacity];
+        }
     }
 
     @Override
     public void add(T value) {
+        ensureCapacity();
         elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
-        if (index < 0 || index > size) {
-            throw new ArrayListIndexOutOfBoundsException(String.format("Index %d is out "
-                    + " of bounds for ArrayList with size %d", index, size));
-        }
+        checkIndexForAdd(index);
+
+        ensureCapacity();
+
         for (int i = size - 1; i >= index; i--) {
             elements[i + 1] = elements[i];
         }
+
         elements[index] = value;
         size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-        elements = (T[]) new Object[list.size() + capacity];
         for (int i = 0; i < list.size(); i++) {
-            elements[i] = list.get(i);
+            add(list.get(i));
         }
-        size = list.size();
     }
 
     @Override
     public T get(int index) {
+        checkIndex(index);
         return elements[index];
     }
 
     @Override
     public void set(T value, int index) {
+        checkIndex(index);
         elements[index] = value;
     }
 
     @Override
     public T remove(int index) {
+        checkIndex(index);
+
         T removedElement = elements[index];
+
         for (int i = index; i < size - 1; i++) {
             elements[i] = elements[i + 1];
         }
-        elements[--size] = null;
-        return removedElement;
 
+        elements[size - 1] = null;
+        size--;
+
+        return removedElement;
     }
 
     @Override
     public T remove(T element) {
         for (int i = 0; i < size; i++) {
-            if (elements[i].equals(element)) {
-                return remove(i);
+
+            if (element == null) {
+                if (elements[i] == null) {
+                    return remove(i);
+                }
+            } else {
+                if (element.equals(elements[i])) {
+                    return remove(i);
+                }
             }
         }
-        throw new NoSuchElementException("Element " + element
-                + " is not found in the ArrayList");
+
+        throw new NoSuchElementException(
+                "Element " + element + " is not found in the ArrayList");
     }
 
     @Override
@@ -84,10 +101,34 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        } else {
-            return false;
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            T[] newElements = (T[]) new Object[elements.length * 2];
+
+            for (int i = 0; i < elements.length; i++) {
+                newElements[i] = elements[i];
+            }
+
+            elements = newElements;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    String.format("Index %d is out of bounds for ArrayList with size %d",
+                            index, size));
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException(
+                    String.format("Index %d is out of bounds for ArrayList with size %d",
+                            index, size));
         }
     }
 }
